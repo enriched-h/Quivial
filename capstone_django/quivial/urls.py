72d7acdd7dc14b0bb264686e1bfbef1e2643283a
Hope_rich <hrkodisang@gmail.com>
2023-04-21 00:55:38 +0200

    Added doctring to the capstone_django/quivial/urls.py

--- capstone_django/quivial/urls.py
+++ capstone_django/quivial/urls.py
@@ -2,10 +2,20 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+"""
+URL patterns for the Quivial app.
+
+This module defines the URL patterns for the Quivial app, including views and URL names.
+
+Attributes:
+    app_name (str): The name of the app.
+
+"""
 
 app_name = 'quivial'
 
 urlpatterns = [
+
     path('', views.index, name='index'),
     path('user_home/', views.user_home, name='user_home'),
     path('play/', views.play, name='play'),
