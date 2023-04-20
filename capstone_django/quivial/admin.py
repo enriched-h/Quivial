eb4e4ecfbf5a7dcaf9fbebb631e15450faa630f3
Hope_rich <hrkodisang@gmail.com>
2023-04-21 01:04:58 +0200

    Made changes to the capstone_django/quivial/admin.py file and added a one line docstring

--- capstone_django/quivial/admin.py
+++ capstone_django/quivial/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import Question, Choice, AttemptedQuestion
 
-
-admin.site.register(Question) 
-admin.site.register(Choice) 
-admin.site.register(AttemptedQuestion) 
 
